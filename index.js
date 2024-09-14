531eeb2549d3b9755b319643300439854f3ce5e7
Nombulelo <nombulelokhumalo48@gmail.com>
2024-09-14 05:35:22 +0200

    Basic form handling and added typewriter

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Either you run the day or the day runs you😉",
+    autoStart: true,
+    cursor: " ",
+    delay: 20,
+  });
+}
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", generatePoem);
+
 // new Typewriter("#typewriter", {
 //   strings: ["Hello", "World"],
 //   autoStart: true,
