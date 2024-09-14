40cb16d1987d8e8fa6c754205bedec8cefaf45e6
Nombulelo <nombulelokhumalo48@gmail.com>
2024-09-14 07:11:26 +0200

    Removed consoles
    Improved user experience. Removed console.logs

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function displayPoem(response) {
-  console.log("quote generated");
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,6 +10,7 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let inputEl = document.querySelector("#input-el");
+  let poem = document.querySelector("#poem");
   let input = inputEl.value.toLowerCase().trim();
 
   let apiKey = "4obaa93faa4d0abd12t05933a7d742d3";
@@ -20,10 +19,9 @@ function generatePoem(event) {
     "You are a motivational speaker and you always have short quotes to motivate people working as they start of their day.User instructions are: Your mission is to generate a short quote. Make sure to follow user instructions. At the end can you skip a line with a <br /> and write , Stay Positive and Enjoy your day😉";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  poem.innerHTML = `Generating quote on " ${input} ".... `;
+
   axios.get(apiUrl).then(displayPoem);
-  console.log("Generating quote");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
 }
 
 let form = document.querySelector("#form");
